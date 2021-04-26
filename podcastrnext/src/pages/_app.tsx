630c032ec5e6aa00a13372b2a0d6bc874c1bbdbc
Natan Xavier <natanxaviersiquera@gmail.com>
2021-04-25 21:18:14 -0300

    Refatorando as funções do play
    Correção de playerContainer para PlayerContext

--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -1,33 +1,14 @@
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { useState } from 'react';
-
-import { playerContainer } from '../Contexts/PlayerContext';
 
 import '../styles/global.scss'
 import styles from '../styles/app.module.scss'
 
+import { PlayerContextProvider } from '../Contexts/PlayerContext'
+
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function TogglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function SetPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
-  return (
-    <playerContainer.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, TogglePlay, SetPlayingState}}>
+ return (
+    <PlayerContextProvider>
     <div className={styles.wrapper}>
       <main>
         <Header />
@@ -35,7 +16,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
-    </playerContainer.Provider>
+    </PlayerContextProvider>
   );
 }
 
